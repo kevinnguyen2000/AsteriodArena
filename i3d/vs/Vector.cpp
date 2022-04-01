76f40979ae3fbc404fd82b276547109952849c72
kevinnguyen2000 <kevinnguyen3047@gmail.com>
2022-04-01 16:12:22 +1100

    Added vector classes.

---
+++ i3d/vs/Vector.cpp
@@ -0,0 +1,12 @@
+#include "Vector.h"
+
+Vector::Vector() {
+
+	this->x = 0;
+	this->y = 0;
+
+}
+
+Vector::~Vector() {
+
+}
---
+++ i3d/vs/Vector.h
@@ -0,0 +1,18 @@
+#ifndef VECTOR
+#define VECTOR
+
+#include "Types.h"
+
+class Vector {
+public:
+	// Constructor / Deconstructor
+	Vector();
+	~Vector();
+
+	// private class variables
+	private:
+		int x;
+		int y;
+};
+
+#endif
