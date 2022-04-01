33e42f5114ee69c69586901dcd4eb9a1ecf54846
kevinnguyen2000 <kevinnguyen3047@gmail.com>
2022-04-01 12:33:16 +1100

    Made shape of player spaceship.

--- i3d/vs/Player.cpp
+++ i3d/vs/Player.cpp
@@ -12,29 +12,33 @@ void Player::display() {
 
 	// pivot point offset
 	const float model_offset = 0.0f;
-	// first vertex - local coordinate
+	// top vertex - local coordinate
 	const float v1x = 0.0f + model_offset;
-	const float v1y = 1.0f + model_offset;
-	// second vertex - local coordinate
-	const float v2x = 1.0f + model_offset;
-	const float v2y = -0.5f + model_offset;
-	// third vertex - local coordinate
-	const float v3x = -1.0f + model_offset;
-	const float v3y = -0.5f + model_offset;
+	const float v1y = 0.0625f + model_offset;
+	// bottom right vertex - local coordinate
+	const float v2x = 0.02f + model_offset;
+	const float v2y = -0.0275f + model_offset;
+	// bottom vertex - local coordinate
+	const float v3x = 0.0f + model_offset;
+	const float v3y = -0.01f + model_offset;
+	// bottom left - local coordinate
+	const float v4x = -0.02f + model_offset;
+	const float v4y = -0.0275f + model_offset;
 
 	// Red triangle - local coordinates
 	// This renders the triangle in its local-coordinate frame
 	glPushMatrix();
 	glLoadIdentity();
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_POLYGON);
+	glColor3f(1.0, 1.0, 1.0);
 	glVertex3f(v1x, v1y, 0.0);
 	glVertex3f(v2x, v2y, 0.0);
 	glVertex3f(v3x, v3y, 0.0);
+	glVertex3f(v4x, v4y, 0.0);
 	glEnd();
 	// this is just so we can see where its pointing
 	glBegin(GL_LINES);
-	glColor3f(1.0, 1.0, 0.0);
+	glColor3f(1.0, 0.0, 0.0);
 	glVertex3f(model_offset, model_offset, 0.0);
 	glVertex3f(v1x, v1y, 0.0);
 	glEnd();
