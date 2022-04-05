8b838a6cbe0daa71dd7ca779b4d63826bae2d0b4
kevinnguyen2000 <kevinnguyen3047@gmail.com>
2022-04-05 13:45:30 +1000

    Updated ship model.

--- i3d/vs/Player.cpp
+++ i3d/vs/Player.cpp
@@ -24,59 +24,52 @@ Player::~Player() {
 
 void Player::display() {
 
-	// pivot point offset
-	const float model_offset = 0.0f;
-	// top vertex - local coordinate
-	const float v1x = 0.0f + model_offset;
-	const float v1y = 0.0625f + model_offset;
-	// bottom right vertex - local coordinate
-	const float v2x = 0.02f + model_offset;
-	const float v2y = -0.0275f + model_offset;
-	// bottom vertex - local coordinate
-	const float v3x = 0.0f + model_offset;
-	const float v3y = -0.01f + model_offset;
-	// bottom left - local coordinate
-	const float v4x = -0.02f + model_offset;
-	const float v4y = -0.0275f + model_offset;
-
 	// Red triangle - local coordinates
 	// This renders the triangle in its local-coordinate frame
 	glPushMatrix();
 	glLoadIdentity();
+
 	//glTranslatef(0.0f, 0.0f, 0.0f);
 	glRotatef(angle, 0.0f, 0.0f, 1.0f);
-	//glScalef(0.2f, 0.2f, 0.2f);
-	glBegin(GL_POLYGON);
-	glColor3f(1.0, 1.0, 1.0);
-	/*
-	glVertex3f(v1x, v1y, 0.0);
-	glVertex3f(v2x, v2y, 0.0);
-	glVertex3f(v3x, v3y, 0.0);
-	glVertex3f(v4x, v4y, 0.0);
-	*/
-	glVertex3f(-10, 0, 0.0);
-	glVertex3f(0, -10, 0.0);
+	glScalef(1.5f, 1.5f, 1.5f);
+
+	glBegin(GL_TRIANGLES);
+	// Front of ship
+	glVertex3f(0, 8, 0.0);
 	glVertex3f(10, 0, 0.0);
-	glVertex3f(0, 10, 0.0);
+	glVertex3f(0, -8, 0.0);
+
+	// Left side of ship
+	glVertex3f(-10, 10, 0.0);
+	glVertex3f(0, 8, 0.0);
+	glVertex3f(0, 0, 0.0);
+
+	// Right side of ship
+	glVertex3f(-10, -10, 0.0);
+	glVertex3f(0, -8, 0.0);
+	glVertex3f(0, 0, 0.0);
+
 	glEnd();
+	
 	// this is just so we can see where its pointing
-	/*
 	glBegin(GL_LINES);
 	glColor3f(0.0, 0.0, 0.0);
-	glVertex3f(model_offset, model_offset, 0.0);
-	glVertex3f(v1x, v1y, 0.0);
+	glVertex3f(4, 0, 0.0);
+	glVertex3f(10, 0, 0.0);
 	glEnd();
+
 	// make outline of spaceship
 	glLineWidth(2.0);
 	glBegin(GL_LINE_LOOP);
-	glColor3f(0.0, 0.5, 1.0);
-	glVertex3f(v1x, v1y, 0.0);
-	glVertex3f(v2x, v2y, 0.0);
-	glVertex3f(v3x, v3y, 0.0);
-	glVertex3f(v4x, v4y, 0.0);
+	glColor3f(0.0, 0.25, 1.0);
+	glVertex3f(-10, 10, 0.0);
+	glVertex3f(0, 8, 0.0);
+	glVertex3f(10, 0, 0.0);
+	glVertex3f(0, -8, 0.0);
+	glVertex3f(-10, -10, 0.0);
+	glVertex3f(0, 0, 0.0);
 
 	glEnd();
-	*/
 	glPopMatrix();
 
 }
